82f8fe8d335bbf44970c70ace97a4019d972f906
fafbr <fafafebrian11@gmail.com>
2025-10-02 10:22:22 +0700

    Modifikasi

--- jobsheet5/ifCetakKRS10.java
+++ jobsheet5/ifCetakKRS10.java
@@ -13,6 +13,9 @@ public class ifCetakKRS10 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
+        sc.close();
     }
 }
