314594f4796f711a1b9ffa210b434d33afcfd923
fafbr <fafafebrian11@gmail.com>
2025-10-02 09:17:10 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS10.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ifCetakKRS10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean uktLunas;
+        
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (true/false: ");
+        uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
