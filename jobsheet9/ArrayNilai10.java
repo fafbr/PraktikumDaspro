d50a69f12ecd6dc65f7896d6e4700bf7be0a328a
fafa <fafafebrian11@gmail.com>
2025-11-05 17:08:17 +0700

    Modifikasi arraNilai

--- jobsheet9/ArrayNilai10.java
+++ jobsheet9/ArrayNilai10.java
@@ -11,7 +11,11 @@
             }
 
             for (int i=0; i <nilaiAkhir.length; i++) {
-                System.out.println("Nilai akhir  ke-"+i+" adalah "+nilaiAkhir[i]);
+                if (nilaiAkhir[i] > 70){
+                    System.out.println("Mahasiswa ke-"+i+" Lulus!");
+                } else {
+                    System.out.println("Mahasiswa ke-"+i+" Tidak Lulus!");
+                }
             }
         }
     }
